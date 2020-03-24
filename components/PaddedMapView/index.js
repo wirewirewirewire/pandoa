c5c80586a2102825e4022078f14d0bedc08d1240
Daniel Böber <danielboe@gmx.de>
2020-03-24 23:50:59 +0100

    fix center button

--- components/PaddedMapView/index.js
+++ components/PaddedMapView/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import MapView from "react-native-maps";
 import { connect } from "react-redux";
 import { getAllPositions } from "../../selectors";
+import { Button, Icon } from "react-native-elements";
+import { StyleSheet } from "react-native";
 
 const EDGE_PADDING = {
   top: 30,
@@ -28,18 +30,57 @@ class PaddedMapView extends Component {
     this.ref.fitToCoordinates(points, options);
   };
 
+  fitToCenter = () => {
+    if (this.props.positions.length === 0) return null;
+
+    const options = {
+      edgePadding: EDGE_PADDING,
+      animated: false
+    };
+
+    const points = this.props.positions.map(point => {
+      return {
+        longitude: point.lng,
+        latitude: point.lat
+      };
+    });
+    this.ref.fitToCoordinates(points, options);
+  };
+
   render() {
     return (
-      <MapView
-        ref={map => {
-          this.ref = map;
-        }}
-        onLayout={this.fitToMarkers}
-        {...this.props}
-      />
+      <>
+        <MapView
+          ref={map => {
+            this.ref = map;
+          }}
+          onLayout={this.fitToMarkers}
+          {...this.props}
+        />
+        <Button
+          onPress={this.fitToCenter}
+          icon={
+            <Icon type="material-community" name="crosshairs-gps" size={25} />
+          }
+          buttonStyle={styles.buttonStyle}
+          containerStyle={styles.containerStyle}
+        />
+      </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: "white",
+    borderRadius: 50
+  },
+  containerStyle: {
+    position: "absolute",
+    top: "12%",
+    right: "7%"
+  }
+});
 
 const mapStateToProps = state => {
   return {
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Dimensions } from "react-native";
 import { connect } from "react-redux";
-import { Button, Icon } from 'react-native-elements';
 import Animated from "react-native-reanimated";
 import MapHistory from "../components/MapHistory";
 import BottomSheetDetails from "../components/BottomSheetDetails";
@@ -9,8 +8,8 @@ import BottomSheetSingle from "../components/BottomSheetSingle";
 import PaddedMapView from "../components/PaddedMapView";
 import TrackHistory from "../components/TrackHistory";
 import { getDetail } from "../selectors";
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 const Screen = {
   width: Dimensions.get("window").width,
@@ -46,9 +45,14 @@ class MapScreen extends Component {
   };
 
   state = {
-    mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
+    mapRegion: {
+      latitude: 51,
+      longitude: 13,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    },
     locationResult: null,
-    location: {coords: { latitude: 37.78825, longitude: -122.4324}},
+    location: { coords: { latitude: 51, longitude: 13 } }
   };
 
   componentDidMount() {
@@ -61,55 +65,34 @@ class MapScreen extends Component {
 
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
+    if (status !== "granted") {
       this.setState({
-        locationResult: 'Permission to access location was denied',
-        location,
+        locationResult: "Permission to access location was denied",
+        location
       });
     }
     let location = await Location.getCurrentPositionAsync({});
-    this.setState({ locationResult: JSON.stringify(location), location, });
-
-
+    this.setState({ locationResult: JSON.stringify(location), location });
   };
-
-  onPress = async () => {
-    console.log("trigger ");
-    let location = await Location.getCurrentPositionAsync({});
-
-      const region = {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.012,
-        longitudeDelta: 0.01
-      };
-
-      console.log("trigger 2 ", region);
-      this.map.animateToRegion(region,1000)
-  };
-
 
   render() {
-    
-    const locateIcon = () => (
-      <Icon
-        type='material-community'
-        name='crosshairs-gps'
-        size={25}
-      />
-    );
     const { detail } = this.props;
     return (
-      <View style={styles.container}>
+      <View
+        style={styles.container}
+        ref={map => {
+          this.map = map;
+        }}
+      >
         <BottomSheetDetails
           detail={detail}
           navigation={this.props.navigation}
         />
         <BottomSheetSingle detail={detail} navigation={this.props.navigation} />
         <PaddedMapView
-          ref={(map) => { this.map = map; }}
           style={{ height: Dimensions.get("window").height - 75 }}
           showsMyLocationButton={false}
+          showsUserLocation={true}
           initialRegion={{
             latitude: this.state.location.coords.latitude,
             longitude: this.state.location.coords.longitude,
@@ -120,12 +103,6 @@ class MapScreen extends Component {
           <MapHistory updateMap={this.updateMap} />
           <TrackHistory updateMap={this.updateMap} />
         </PaddedMapView>
-        <Button
-            onPress={this.onPress}
-            icon={locateIcon}
-            buttonStyle={styles.buttonStyle}
-            containerStyle={styles.containerStyle}
-          />
       </View>
     );
   }
@@ -133,16 +110,8 @@ class MapScreen extends Component {
 
 const styles = StyleSheet.create({
   buttonStyle: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 50
-  },
-  container: {
-    flex: 1
-  },
-  containerStyle: {
-    position: 'absolute',
-    top: '12%',
-    right: '7%',
   }
 });
 
